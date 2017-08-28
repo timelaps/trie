80e7d24feb166278bb5955228ce1846c9891bb66
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-08-27 20:17:36 -0500

    updated array implementation

--- array/index.js
+++ array/index.js
@@ -2,6 +2,7 @@ module.exports = TrieMaker;
 TrieMaker.constructor = Trie;
 var u;
 var MAX = 32;
+TrieMaker.MAX = MAX;
 var fromTo = require('@timelaps/n/from/to');
 var isUndefined = require('@timelaps/is/undefined');
 var assign = require('@timelaps/object/assign');
@@ -11,42 +12,93 @@ var isArray = require('@timelaps/is/array');
 var isNumber = require('@timelaps/is/number');
 var clamp = require('@timelaps/number/clamp');
 var isString = require('@timelaps/is/string');
+var bindTo = require('@timelaps/fn/bind/to');
+var toArrayFromArrayLike = require('@timelaps/to/array/from/array-like');
+var concat = require('@timelaps/array/concat');
+var toArray = require('@timelaps/to/array');
+var cacheable = require('@timelaps/fn/cacheable');
+var computeCapacity = cacheable(computesCapacity);
+var computeDepth = cacheable(computesDepth);
+var computeBlockSize = cacheable(computesBlockSize);
+var base32ToIndex = cacheable(base32ToIndexer);
 Trie.prototype = {
-    fromTo: function (fn, memo, start, end, blockSize) {
+    constructor: Trie,
+    fromTo: function (fn, memo, start, end) {
         var length = this.length;
         return deepRead(fromTo, this.value, fn, memo, start || 0, end || length, length, this.depth);
     },
     reduce: function (fn, memo) {
-        return this.fromTo(fn, memo);
+        return this.fromTo(bindTo(fn, this), memo);
+    },
+    copy: function () {
+        return new Trie({
+            depth: this.depth,
+            length: this.length,
+            value: this.value.slice(0)
+        });
+    },
+    forEach: bindFirst(function (trie, fn) {
+        // drop all memos
+        trie.fromTo(function (memo, value, index) {
+            fn(value, index, trie);
+        });
+        return trie;
+    }),
+    map: bindFirst(function (trie, fn) {
+        return trie.fromTo(function (memo, value, index) {
+            //
+        });
+    }),
+    push: function () {
+        // use a super optimized route for one
+        return concatSome(this, toArrayFromArrayLike(arguments));
+    },
+    concat: function () {
+        return concatSome(this, concat(toArrayFromArrayLike(arguments)));
     },
     get: function (index) {
         return index < this.length ? get(this, this.toIndexPath(index)) : u;
     },
-    ofMutable: function (fn, memo) {
-        var mutated, trie = this,
-            readable = trie.value;
-        return fn(trie, readable, mutable) || trie;
-
-        function mutable() {
-            // only make it once
-            return mutated || (mutated = readable.slice(0));
+    fromToWrite: function (inclusiveMin, exclusiveMax, fn) {
+        return this.ofMutable(function (trie) {
+            //
+        });
+    },
+    write: function (index, fn) {
+        return this.ofMutable(this.depth ? function (trie) {
+            var subtrie = trie.access(index);
+            var sub = subtrie || trie.subtrie();
+            var next = nextChunk(index);
+            var updated = sub.write(next, fn);
+            // if (updated !== )
+        } : function (trie) {
+            //
+        });
+    },
+    mutable: function () {
+        return this.mutated || (this.mutated = this.copy());
+    },
+    ofMutable: function (fn) {
+        var mutated, trie = this;
+        trie.mutating += 1;
+        var result = fn(trie);
+        trie.mutating -= 1;
+        if (!trie.mutating && (mutated = result || trie.mutated)) {
+            delete trie.mutated;
+            return mutated;
         }
+        return trie;
     },
     set: function (index, item) {
-        var indexPath = this.toIndexPath(index);
-        var method = index < this.capacity() ? setWithinCapacity : setOutsideOfCapacity;
-        return method(this, indexPath, item);
-    },
-    toIndexPath: function (index) {
-        return toIndexPath(index, this.depth);
-    },
-    capacity: function () {
-        return computeCapacity(this.depth);
+        var trie = this;
+        var indexPath = trie.toIndexPath(index);
+        var method = index < trie.capacity ? setWithinCapacity : setOutsideOfCapacity;
+        return method(trie, indexPath, item);
     },
     subtrie: function () {
         return new Trie({
-            length: 0,
             depth: this.depth - 1,
+            length: 0,
             value: appropriatelySized()
         });
     },
@@ -54,21 +106,160 @@ Trie.prototype = {
         var list = appropriatelySized();
         list[0] = this;
         return new Trie({
-            length: this.length,
             depth: this.depth + 1,
+            length: this.length,
             value: list
         });
+    },
+    bunchUntil: function (length) {
+        var trie = this,
+            result = trie,
+            neededDepth = computeDepth(length),
+            currentDepth = trie.depth;
+        while (currentDepth < neededDepth) {
+            result.length = result.capacity;
+            result = result.bunch();
+            currentDepth = result.depth;
+        }
+        return result;
+    },
+    access: function (index) {
+        return this.value[base32ToIndex(index)];
+    },
+    toIndexPath: function (index) {
+        return toIndexPath(index, this.depth);
+    },
+    lastIndex: function () {
+        return base32ToIndex(this.toIndexPath(this.length - 1)[0]);
     }
 };
+
+function concatSome(trie, some) {
+    var length = some.length;
+    if (length > 1) {
+        return concatMany(trie, some);
+    } else if (length === 1) {
+        return concatOne(trie, some[0]);
+    } else {
+        return trie;
+    }
+}
+
+function fillChildren(trie, array, start_, end_, indexPath) {
+    var concatable, added, concatableValue, end = end_,
+        start = start_,
+        index = base32ToIndex(indexPath[0]),
+        next = nextChunk(indexPath),
+        value = trie.value[index],
+        baseline = index,
+        block = trie.block,
+        exclusiveMax = array.length,
+        capacity = trie.capacity,
+        length = trie.length,
+        absoluteMax = clamp(array.length, 0, capacity - trie.length),
+        mutable = trie.mutable();
+    if (value) {
+        end = clamp(end, 0, value.capacity - value.length);
+        start = fillToCapacity(value, array, start, end, next);
+        mutable.value[index] = value.mutable();
+        baseline += 1;
+    }
+    if (start >= absoluteMax) {
+        mutable.length = trie.length + start;
+        return start;
+    }
+    concatable = churn(array, start, exclusiveMax, absoluteMax, trie.depth, start);
+    added = start + concatable.length;
+    concatableValue = concatable.value;
+    fromTo(function (index, value) {
+        value[baseline + index] = concatableValue[index];
+        return value;
+    }, mutable.value, 0, concatable.lastIndex(), 1);
+    mutable.length = trie.length + added;
+    return added;
+}
+
+function fillToCapacity(trie, array, start, end, indexPath) {
+    var method = trie.depth ? fillChildren : fillShallow;
+    return method(trie, array, start, end, indexPath);
+}
+
+function fillShallow(trie, array, start, end, begin) {
+    var mutable = trie.mutable(),
+        offset = base32ToIndex(begin || 0),
+        value = mutable.value,
+        length = mutable.length,
+        added = end - start;
+    if (!added) {
+        return added;
+    }
+    fromTo(function (index, memo) {
+        memo[offset + index] = array[index];
+        return memo;
+    }, value, start, end - 1, 1);
+    mutable.length = length + added;
+    return added;
+}
+
+function fillOverCapacity(trie, array, start, end) {
+    var nextLength = trie.length + end - start;
+    var nextTrie = trie.bunchUntil(nextLength);
+    var indexPath = nextTrie.toIndexPath(trie.length);
+    fillToCapacity(nextTrie, array, start, end, indexPath);
+    return nextTrie.mutable();
+}
+
+function concatOne(trie, next) {
+    // too simple
+    return trie.set(trie.length, next);
+}
+
+function concatOneAlternative(trie, next) {
+    var length = trie.length;
+    var block = trie.block;
+    if (block !== 1 && length % block) {
+        // needs a whole new block
+    } else if (length % MAX === 0) {
+        // needs a new one added on
+    } else {
+        // it can fit more in
+    }
+}
+
+function concatMany(trie, extension) {
+    var start = 0,
+        baseline = trie,
+        length = baseline.length,
+        capacity = baseline.capacity,
+        final = extension.length,
+        end = clamp(capacity - length, 0, final);
+    return trie.ofMutable(function (trie) {
+        var added, mutable, indexPath;
+        if (end) {
+            indexPath = trie.toIndexPath(trie.length);
+            added = fillToCapacity(trie, extension, 0, end, indexPath);
+        }
+        if (end < final) {
+            // returns the bunched version
+            return fillOverCapacity(trie, extension, end, final);
+        }
+    });
+}
+
+function bindFirst(fn) {
+    return function (runner) {
+        return fn(trie, bindTo(runner, this));
+    };
+}
 
 function TrieMaker(original_) {
     var original = original_ || [];
     var length = original.length;
     var depth = computeDepth(length);
-    return churn(original, 0, length, length, depth);
+    return churn(original, 0, length, length, depth, 0);
 }
 
-function base32ToIndex(binary) {
+function base32ToIndexer(binary) {
     return parseInt(binary, MAX);
 }
 
@@ -80,43 +271,44 @@ function toBase32(number) {
     return parseInt(number).toString(MAX);
 }
 
-function keepChurning(original, inclusiveMin, exclusiveMax, length, depth) {
-    var blockSize = Math.pow(MAX, depth);
+function keepChurning(original, inclusiveMin, exclusiveMax, length, depth, offset) {
+    var block = computeBlockSize(depth);
     return fromTo(function (index, memo) {
-        var idx = (index / blockSize) % MAX;
-        memo[idx] = churn(original, index, index + blockSize, length, depth - 1);
+        var idx = ((index - offset) / block) % MAX;
+        memo[idx] = churn(original, index, clamp(index + block, 0, length), length, depth - 1, offset);
         return memo;
-    }, appropriatelySized(), inclusiveMin, clamp(exclusiveMax, 0, length) - 1, blockSize);
+    }, appropriatelySized(), inclusiveMin, clamp(exclusiveMax, 0, length) - 1, block);
 }
 
 function slice(original, inclusiveMin, exclusiveMax) {
     return original.slice(inclusiveMin, exclusiveMax);
 }
 
-function churn(original, inclusiveMin, exclusiveMax, length, depth) {
+function churn(original, inclusiveMin, exclusiveMax, length, depth, offset) {
     var next;
-    if (exclusiveMax - inclusiveMin > MAX) {
-        next = keepChurning(original, inclusiveMin, exclusiveMax, length, depth);
+    if (depth || exclusiveMax - inclusiveMin > MAX) {
+        next = keepChurning(original, inclusiveMin, exclusiveMax, length, depth, offset);
     } else {
-        next = slice(original, inclusiveMin, exclusiveMax);
+        next = expandList(original, inclusiveMin, exclusiveMax);
     }
     return new Trie({
-        length: exclusiveMax - inclusiveMin,
         depth: depth,
+        length: exclusiveMax - inclusiveMin,
         value: next
     });
 }
 
 function keepDeepReading(fromTo, structure, fn, memo, inclusiveMin, exclusiveMax, length, depth) {
-    var blockSize = Math.pow(MAX, depth);
+    var block = computeBlockSize(depth);
     return fromTo(function (index, memo) {
-        var idx = (index / blockSize) % MAX;
-        var trie = structure[idx];
+        var idx = (index / block) % MAX,
+            trie = structure[idx];
         if (!trie) {
             return memo;
         }
-        return deepRead(fromTo, trie.value, fn, memo, index, index + blockSize, length, depth - 1);
-    }, memo, inclusiveMin, clamp(exclusiveMax, 0, length) - 1, blockSize);
+        var exclusiveMax = clamp(index + block, 0, length);
+        return deepRead(fromTo, trie.value, fn, memo, index, exclusiveMax, length, depth - 1);
+    }, memo, inclusiveMin, clamp(exclusiveMax, 0, length) - 1, block);
 }
 
 function largerThanBlock(number) {
@@ -138,72 +330,76 @@ function deepRead(fromTo, structure, fn, memo, inclusiveMin, exclusiveMax, lengt
 }
 
 function setOutsideOfCapacity(trie, indexPath, value) {
-    var index = base32ToIndex(indexPath);
-    var length = index + 1;
-    var currentDepth = trie.depth;
-    var neededDepth = computeDepth(length);
-    var result = trie;
-    while (currentDepth < neededDepth) {
-        result.length = result.capacity();
-        result = result.bunch();
-        currentDepth = result.depth;
-    }
+    var index = base32ToIndex(indexPath),
+        length = index + 1,
+        result = trie.bunchUntil(length);
     result.length = length;
     return setWithinCapacity(result, indexPath, value);
 }
 
 function setWithinCapacity(trie, indexPath, value) {
     // modify the trie
-    var next, first = indexPath[0],
-        index = base32ToIndex(first);
-    return trie.ofMutable(trie.depth ? function (trie, readable, mutable) {
-        var result = readable;
-        var subTrie = readable[index];
-        var next = nextChunk(indexPath);
-        var sub = subTrie || trie.subtrie();
-        var afterSet = setWithinCapacity(sub, next, value);
+    var first = indexPath[0],
+        index = base32ToIndex(first),
+        next = nextChunk(indexPath),
+        minLength = base32ToIndex(indexPath) + 1;
+    return trie.ofMutable(trie.depth ? function (trie) {
+        var result = trie,
+            subTrie = trie.value[index],
+            sub = subTrie || trie.subtrie(),
+            length = sub.length || computeBlockSize(sub.depth) * index,
+            afterSet = setWithinCapacity(sub, next, value);
         if (subTrie !== afterSet) {
-            result = mutable();
-            result[index] = afterSet;
-            return new Trie({
-                length: trie.length,
-                depth: trie.depth,
-                value: result
-            });
+            result = trie.mutable();
+            result.value[index] = afterSet;
+            result.length = clamp(length, minLength);
         }
-    } : function (trie, readable, mutable) {
-        var length, result;
-        if (readable[index] !== value) {
-            result = mutable();
-            result[index] = value;
-            length = clamp(trie.length, index + 1);
-            return new Trie({
-                length: length,
-                depth: trie.depth,
-                value: result
-            });
+    } : function (trie) {
+        var result = trie,
+            current = trie.value[index];
+        if (current !== value) {
+            result = trie.mutable();
+            result.value[index] = value;
+            result.length = clamp(trie.length, index + 1);
         }
     });
 }
 
 function get(trie, indexPath) {
-    var next, first = indexPath[0],
-        sub = trie.value[base32ToIndex(first)];
+    var sub = trie.access(indexPath[0]);
     return trie.depth ? get(sub, nextChunk(indexPath)) : sub;
 }
 
-function Trie(options, structure) {
+function Trie(options) {
     var trie = this;
-    assign(trie, options);
+    trie.value = options.value;
+    trie.length = options.length;
+    var depth = trie.depth = options.depth;
+    trie.block = computeBlockSize(depth);
+    trie.capacity = computeCapacity(depth);
     trie.mutating = 0;
 }
 
-function computeCapacity(depth) {
-    return Math.pow(MAX, depth + 1);
+function expandList(list, from, to) {
+    if (to - from === MAX) {
+        return list.slice(from, to);
+    }
+    return fromTo(function (index, memo) {
+        memo[index - from] = list[index];
+        return memo;
+    }, appropriatelySized(), from, to - 1, 1);
 }
 
-function computeDepth(length) {
+function computesCapacity(depth) {
+    return MAX * computesBlockSize(depth);
+}
+
+function computesDepth(length) {
     return length > MAX ? 1 + computeDepth(length / MAX) : 0;
+}
+
+function computesBlockSize(depth) {
+    return Math.pow(MAX, depth);
 }
 
 function appropriatelySized(length) {
@@ -211,10 +407,10 @@ function appropriatelySized(length) {
 }
 
 function iterate(trie, level, fn, memo, index, depth) {
-    var blockSize = Math.pow(MAX, depth);
+    var block = computeBlockSize(depth);
     return fromTo(function (index, memo) {
         return !depth ? fn(index, trie, memo) : iterate(trie, level, fn, memo, index, depth);
-    }, memo, index, index + (level.length * blockSize) - 1, blockSize);
+    }, memo, index, index + (level.length * block) - 1, block);
 }
 
 function binaryToPathIndex(index, depth) {
--- array/index.test.js
+++ array/index.test.js
@@ -8,6 +8,21 @@ b.describe('Trie', function () {
         var trie = Trie([]);
         t.expect(trie).toBeInstance(Trie.constructor);
     });
+    b.describe('structure', function () {
+        b.it('has a value property that points to an array', function (t) {
+            var trie = Trie();
+            t.expect(trie.value).toBeArray();
+        });
+        b.it('the value array always have a length that matches the exposed MAX', function (t) {
+            var trie = Trie([2, 3, 4]);
+            t.expect(trie.value.length).toBe(Trie.MAX);
+        });
+        b.it('will have the correct sized elements even in deep structures', function (t) {
+            var trie = Trie(make(100));
+            var sub = trie.access(3);
+            t.expect(sub.length).toBe(100 % Trie.MAX);
+        });
+    });
     b.describe('methods', function () {
         b.describe('get', function () {
             b.it('allows you to access items', function (t) {
@@ -63,10 +78,7 @@ b.describe('Trie', function () {
                 });
             }, 9);
             b.it('will iterate through large structures', function (t) {
-                var array = new Array(1000000);
-                for (var i = 0; i < array.length; i++) {
-                    array[i] = i;
-                }
+                var array = make(1000000);
                 var trie = Trie(array);
                 var trieSum = trie.reduce(sum, 0);
                 var arraySum = array.reduce(sum, 0);
@@ -77,5 +89,99 @@ b.describe('Trie', function () {
                 }
             });
         });
+        b.describe('push', function () {
+            b.it('adds an element to the end of the trie', function (t) {
+                var array = make(31);
+                var sparseTrie = Trie(array);
+                var fullTrie = sparseTrie.push(31);
+                t.expect(fullTrie).notToBe(sparseTrie);
+                t.expect(fullTrie.length).toBe(32);
+                t.expect(fullTrie.get(31)).toBe(31);
+                t.expect(sparseTrie.get(31)).toBeUndefined();
+            }, 4);
+            b.it('can add elements to a deep trie', function (t) {
+                var nextIndex = 32 * 4;
+                var array = make(nextIndex);
+                var trie = Trie(array);
+                var moreTrie = trie.push(nextIndex);
+                t.expect(moreTrie.get(nextIndex)).toBe(nextIndex);
+                t.expect(moreTrie.length).toBe(nextIndex + 1);
+                t.expect(trie.length).toBe(nextIndex);
+            }, 3);
+            b.it('can bunch elements', function (t) {
+                var trie = Trie(make(32));
+                var moreTrie = trie.push(32);
+                t.expect(trie).notToBe(moreTrie);
+                t.expect(trie.get(32)).toBeUndefined();
+                t.expect(moreTrie.get(32)).toBe(32);
+            }, 3);
+            b.it('doesn\'t leave extra undefined in the array', function (t) {
+                var nextIndex = 32 * 32;
+                var trie = pushTo(Trie(), nextIndex);
+                var moreTrie = trie.push(nextIndex);
+                t.expect(moreTrie.get(nextIndex)).toBe(nextIndex);
+                t.expect(trie).notToBe(moreTrie);
+                t.expect(trie.get(nextIndex)).toBeUndefined();
+            }, 3);
+        });
+        b.describe('concat', function () {
+            b.it('can add many elements to the end of a trie', function (t) {
+                var sparseTrie = Trie(make(10));
+                var moreTrie = sparseTrie.concat([10, 11, 12, 13, 14]);
+                t.expect(moreTrie).notToBe(sparseTrie);
+                t.expect(moreTrie.get(11)).toBe(11);
+                t.expect(sparseTrie.get(11)).toBeUndefined();
+            }, 3);
+            b.it('can add elements to a sparce and deep trie', function (t) {
+                var nextIndex = 32 * 4.5;
+                var thirtyTwoFive = 32 * 6.5;
+                var array = make(nextIndex);
+                var trie = Trie(array);
+                var set = make(thirtyTwoFive).slice(nextIndex);
+                var moreTrie = trie.concat(set);
+                t.expect(moreTrie.get(thirtyTwoFive - 1)).toBe(thirtyTwoFive - 1);
+                t.expect(moreTrie.length).toBe(thirtyTwoFive);
+                t.expect(trie.length).toBe(nextIndex);
+            }, 3);
+        });
     });
 });
+benchmarks();
+
+function pushTo(trie, limit) {
+    var list = trie;
+    for (var i = 0; i < limit; i++) {
+        list = list.push(i);
+    }
+    return list;
+}
+
+function benchmarks() {
+    var list = Trie();
+    benchmark(function () {
+        list = pushTo(list, 1000000);
+    });
+    benchmark(function () {
+        b.log(list.reduce(function (memo, next) {
+            return memo + next;
+        }, 0));
+    });
+}
+
+function benchmark(fn) {
+    var start = process.uptime();
+    fn();
+    b.log(process.uptime() - start);
+}
+
+function capacityByDepth(depth) {
+    return Math.pow(32, depth + 1);
+}
+
+function make(capacity) {
+    var empty = new Array(capacity);
+    for (var i = 0; i < capacity; i++) {
+        empty[i] = i;
+    }
+    return empty;
+}
